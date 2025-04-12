fb842bf6200801b922fc62ef955b064ad310ce03
SpencerCooley07 <spencerjcooley07@gmail.com>
2025-04-12 22:19:04 +1000

    Simplified Habit Tracker logic

--- habit_tracker.py
+++ habit_tracker.py
@@ -1,17 +1,27 @@
-from habit import Habit
-from database import Database
+from datetime import date
 import json
 import os
 
+class Database:
+    def __init__(self, filename="data/database.json"):
+        self.filename = filename
+        
+    def load_data(self):
+        if not os.path.exists(self.filename): return {}
+        with open(self.filename, "r") as file: data = json.load(file)
+        return data
+
+
+
 class HabitTracker():
 	def __init__(self) -> None:
-		self.db = Database('data/database.json').load_data()
-		print(self.db)
-		self.habits = {}
+		self.db: dict = Database('data/database.json').load_data()
 
-	def add_habit(self, name: str, description: str, created: str, habit_type: str, goal: int, unit: str, tags: list[str]) -> None:
-		if name not in self.db:
-			self.habits[name] = Habit(name, description, created, habit_type, goal, unit, tags)
+	def debug_display(self) -> None:
+		for name, data in self.db.items(): print(f'{name}\n{data}\n')
+
+
 
 if __name__ == "__main__":
 	ht = HabitTracker()
+	ht.debug_display()
