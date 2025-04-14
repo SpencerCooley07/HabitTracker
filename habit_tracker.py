43418e78b217706a60ca00ad231c966e1be6d5a9
SpencerCooley07 <spencerjcooley07@gmail.com>
2025-04-14 22:24:08 +1000

    Finalised HabitTracker

--- habit_tracker.py
+++ habit_tracker.py
@@ -28,7 +28,7 @@ class HabitTracker:
 			"habit_type": habit_type,
 			"goal": goal,
 			"unit": unit,
-			"tags": tags,
+			"tags": sorted(tags, key=str.lower),
 			"streak": {"current": 0, "longest": 0} if streak == None else streak,
 			"archived": archived,
 			"entries": [] if entries == None else entries
@@ -41,11 +41,14 @@ class HabitTracker:
 
 	def habit_add_entry(self, name: str, date: str, value: int | float | bool, note: str = "") -> None:
 		entries = self.data[name]["entries"]
+
+		if (type(value) == int or float) and (self.data[name]["habit_type"] == "bool"): return
+		if (type(value) == bool) and (self.data[name]["habit_type"] == "numeric"): return
 		
 		for i, entry in enumerate(entries):
 			if date < entry["date"]:
 				entries.insert(i, {"date": date, "value": value, "note": note})
-				self.database.save_data(self.data)
+				self.habit_update_streak(name)
 				return
 
 		entries.append({"date": date, "value": value, "note": note})
@@ -64,7 +67,6 @@ class HabitTracker:
 		entries = self.data[name]["entries"]
 		dates = [datetime.strptime(entry["date"], "%Y-%m-%d").toordinal() for entry in entries]
 		current_streak, longest_streak = 1, 1
-
 		for i in range(1, len(dates)):
 			if dates[i] - dates[i-1] == 1: current_streak += 1
 			else:
@@ -72,4 +74,47 @@ class HabitTracker:
 				current_streak = 1
 
 		self.data[name]["streak"] = {"current": current_streak, "longest": max(current_streak, longest_streak)}
+		self.database.save_data(self.data)
+
+	def habit_update_name(self, name: str, new_name: str) -> None:
+		self.data[new_name] = self.data.pop(name)
+		self.database.save_data(self.data)
+
+	def habit_update_description(self, name: str, new_description: str) -> None:
+		self.data[name]["description"] = new_description
+		self.database.save_data(self.data)
+
+	def habit_update_goal(self, name: str, new_goal: int | float | bool) -> None:
+		old_goal = self.data[name]["goal"]
+		self.data[name]["goal"] = new_goal
+		if (type(old_goal) == int or float) and (type(new_goal) == bool): self.data[name]["entries"].clear()
+		if (type(old_goal) == bool) and (type(new_goal) == int or float): self.data[name]["entries"].clear()
+
+		if type(new_goal) == bool:
+			self.data[name]["habit_type"] = "bool"
+			self.habit_update_unit(name, None)
+			return
+
+		self.data[name]["habit_type"] = "numeric"
+		self.database.save_data(self.data)
+
+	def habit_update_unit(self, name: str, new_unit: str | None) -> None:
+		self.data[name]["unit"] = new_unit
+		self.database.save_data(self.data)
+
+	def habit_update_tags(self, name: str, new_tags: str) -> None:
+		self.data[name]["tags"] = new_tags
+		self.database.save_data(self.data)
+
+	def habit_add_tags(self, name: str, new_tags: list[str]) -> None:
+		self.data[name]["tags"] += new_tags
+		self.data[name]["tags"].sort(key=str.lower)
+		self.database.save_data(self.data)
+
+	def habit_del_tags(self, name: str, tag: str) -> None:
+		self.data[name]["tags"].remove(tag)
+		self.database.save_data(self.data)
+
+	def habit_toggle_archive(self, name: str) -> None:
+		self.data[name]["archived"] = not self.data[name]["archived"]
 		self.database.save_data(self.data)
