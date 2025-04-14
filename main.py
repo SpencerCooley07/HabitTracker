6931a9f722233b85aab2c738205c05674b2b295d
SpencerCooley07 <spencerjcooley07@gmail.com>
2025-04-14 23:38:32 +1000

    Rough base idea

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from habit_tracker import HabitTracker
 
-import tkinter
+import tkinter as tk
 from tkinter import ttk
 
 import sv_ttk
 import darkdetect
 import pywinstyles, sys
 
+# MISC FUNCTIONS
 def apply_theme_to_titlebar(root):
     version = sys.getwindowsversion()
 
@@ -20,13 +21,26 @@ def toggle_theme(root):
 	sv_ttk.toggle_theme(root)
 	apply_theme_to_titlebar(root)
 
-root = tkinter.Tk()
+
+
+# PROGRAM LOGIC
+ht = HabitTracker()
+habit_list = ht.get_habit_names()
 
 
 
-button = ttk.Button(root, text="Click me!")
-button.pack()
+# GUI
+root = tk.Tk()
 
+root.title("Main Window")
+root.geometry("1920x1080")
+
+habit = tk.StringVar(root, habit_list[0])
+habit_select = ttk.OptionMenu(root, habit, habit_list[0], *habit_list, command= lambda value: print(f'Selected {value}'))
+habit_select.place(x = 10, y = 10, height = 50, width = 400)
+
+habit_desc = tk.Entry(root)
+habit_desc.place(x=10, y=75, height = 20, width=400)
 
 
 sv_ttk.set_theme("dark" if darkdetect.isDark() else "light")
