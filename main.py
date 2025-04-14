6ad731634811fd98be47e9572389bf4b5a99db3c
SpencerCooley07 <spencerjcooley07@gmail.com>
2025-04-14 19:14:57 +1000

    Shows all current HabitTracker functionality

--- main.py
+++ main.py
@@ -3,3 +3,7 @@ from habit_tracker import HabitTracker
 main = HabitTracker()
 main.add_habit("Running", "Run a kilometre a day", "2025-04-1", "numeric", 1, "km", ["Health", "Fitness", "Cardio"])
 main.add_habit("Journal", "Journal every night", "2025-04-1", "bool", True, None, ["Health", "Fitness", "Cardio"])
+main.habit_add_entry("Running", "2025-04-13", 2.27)
+main.habit_add_entry("Running", "2025-04-14", 2.56)
+main.habit_del_entry("Running", "2025-04-13")
+main.habit_add_entry("Journal", "2025-04-14", True)
