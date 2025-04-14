1d1776f262d0fe5bb6df1839a1606c91d3d8381a
SpencerCooley07 <spencerjcooley07@gmail.com>
2025-04-14 19:15:40 +1000

    Added ability to add and delete habits and fixed streak + entries mutability issue

--- habit_tracker.py
+++ habit_tracker.py
@@ -21,7 +21,7 @@ class HabitTracker:
 		self.database = Database("data/database.json")
 		self.data = self.database.load_data()
 
-	def add_habit(self, name: str, description: str, created: str, habit_type: str, goal, unit, tags: list[str], streak: dict[str, float] = {"current": 0, "longest": 0}, archived: bool = False, entries: list[dict] = []):
+	def add_habit(self, name: str, description: str, created: str, habit_type: str, goal: int | float | bool, unit: str | None, tags: list[str], streak: dict[str, float] | None = None, archived: bool = False, entries: list[dict] | None = None) -> None:
 		self.data[name] = {
 			"description": description,
 			"created": created,
@@ -29,12 +29,33 @@ class HabitTracker:
 			"goal": goal,
 			"unit": unit,
 			"tags": tags,
-			"streak": streak,
+			"streak": {"current": 0, "longest": 0} if streak == None else streak,
 			"archived": archived,
-			"entries": entries
+			"entries": [] if entries == None else entries
 		}
 		self.database.save_data(self.data)
 
-	def delete_habit(self, name: str):
+	def delete_habit(self, name: str) -> None:
 		del self.data[name]
+		self.database.save_data(self.data)
+
+	def habit_add_entry(self, name: str, date: str, value: int | float | bool, note: str = "") -> None:
+		entries = self.data[name]["entries"]
+		
+		for i, entry in enumerate(entries):
+			if date < entry["date"]:
+				entries.insert(i, {"date": date, "value": value, "note": note})
+				self.database.save_data(self.data)
+				return
+
+		entries.append({"date": date, "value": value, "note": note})
+		self.database.save_data(self.data)
+
+	def habit_del_entry(self, name: str, date: str) -> None:
+		entries = self.data[name]["entries"]
+
+		for i, entry in enumerate(entries):
+			if date == entry["date"]:
+				del entries[i]
+
 		self.database.save_data(self.data)
