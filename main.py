a460b1f8d2d46f6d6b5a498baa23a0bf02b2e9ec
SpencerCooley07 <spencerjcooley07@gmail.com>
2025-04-15 17:05:09 +1000

    Placeholder save button and functioning delete button

--- main.py
+++ main.py
@@ -78,20 +78,61 @@ class HabitApp(tk.Tk):
         self.unit_var = tk.StringVar()
         self.tags_var = tk.StringVar()
 
-
-
         self.init_widgets()
-
-
 
     def init_widgets(self) -> None:
         # Habit Selection
-        habit_names = self.tracker.get_habit_names()
+        
         self.habit_dropdown = ttk.Combobox(self.left_frame, textvariable=self.selected_habit, state="readonly")
+        self.habit_dropdown.bind("<<ComboboxSelected>>", self.update_fields)
         self.habit_dropdown.grid(column=0, row=0, padx=10, pady=10, sticky="ew")
+        self.update_dropdown()
+        
+        # Habit Name
+        ttk.Label(self.left_frame, text = "Habit Name").grid(column=0, row=1, sticky="w", padx=10, pady=(10,5))
+        self.name_entry = ttk.Entry(self.left_frame, textvariable=self.name_var)
+        self.name_entry.grid(column=0, row=2, sticky="ew", padx=10, pady=5)
+
+        # Save
+        self.save_button = ttk.Button(self.left_frame, text="Save Changes")
+        self.save_button.grid(column=0, row=3, sticky="ew", padx=10, pady=(20,5))
+
+        # Delete
+        self.delete_style = ttk.Style()
+        self.delete_style.configure("Delete.TButton", foreground="red")
+
+        self.delete_button = ttk.Button(self.left_frame, text="DELETE HABIT")
+        self.delete_button.grid(column=0, row=4, sticky="ew", padx=10, pady=5)
+        self.delete_button.config(style="Delete.TButton")
+        self.delete_button.bind("<ButtonRelease-1>", self.delete)
+
+    def update_dropdown(self) -> None:
+        habit_names = self.tracker.get_habit_names()
         self.habit_dropdown["values"] = ["Add Habit"] + habit_names
         if habit_names: self.selected_habit.set(habit_names[0])
         else: self.selected_habit.set("Add Habit")
+        self.update_fields()
+
+    def update_fields(self, event=None) -> None:
+        name = self.selected_habit.get()
+
+        if name != "Add Habit":
+            data = self.tracker.get_habit_data(name)
+
+            self.name_var.set(name)
+
+    def delete(self, event=None) -> None:
+        name = self.selected_habit.get()
+
+        if name != "Add Habit":
+            if not messagebox.askokcancel("Delete Habit Confirmation", "Are you sure you wish to delete this habit?\nAll data will be lost."):
+                return
+            self.tracker.delete_habit(name)
+            self.update_dropdown()
+
+        else:
+            self.selected_habit.set(self.tracker.get_habit_names()[0])
+            self.update_fields()
 
 
 
